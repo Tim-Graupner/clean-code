cb31a7a0531bb7e20176567df2382d67b2fe4d5c
Graupner <Timothy.Graupner@adesa.com>
2019-04-12 15:19:35 -0400

    Cleaned up code
    Cleaned the code, by moving large reused sections of text into single variables used multiple times

--- src/main/java/briscoe/cleancode/VehicleFormatter.java
+++ src/main/java/briscoe/cleancode/VehicleFormatter.java
@@ -2,6 +2,18 @@ package briscoe.cleancode;
 
 public class VehicleFormatter {
 
+	public static final String HTML_TEMPLATE = "<h1>Vehicle Details</h1>\n" +
+	"<table>\n" +
+	"<tr><th>VIN</th><th>Blacklisted</th><th>Make</th><th>Model</th><th>Year</th><th>color</th><th>Miles</th></tr>\n" +
+	"<tr>\n" +
+	"<td>%1$s</td><td>%2$s</td><td>%3$s</td><td>%4$s</td><td>%5$s</td><td>%6$s</td><td>%7$s</td>\n" +
+	"</tr>\n" +
+	"</table>";
+
+	public static final String MARKDOWN_TEMPLATE = "# Vehicle Details\n" +
+	"VIN | Blacklisted | Make | Model | Year | color | Miles\n" +
+	"--- | --- | --- | --- | --- | --- | ---\n" +
+	"%1$s | %2$s | %3$s | %4$s | %5$s | %6$s | %7$s";
 	private final Vehicle vehicle;
 
 	public static VehicleFormatter byVin(final String vin) {
@@ -17,28 +29,22 @@ public class VehicleFormatter {
 	}
 
 	private String render(final String template) {
-		return String.format(template, 
-				vehicle.getVin(), 
-				vehicle.isBlacklisted(), 
-				vehicle.getMake(), 
+		return String.format(template,
+				vehicle.getVin(),
+				vehicle.isBlacklisted(),
+				vehicle.getMake(),
 				vehicle.getModel(),
-				vehicle.getYear(), 
-				vehicle.getColor(), 
+				vehicle.getYear(),
+				vehicle.getColor(),
 				vehicle.getMiles());
 	}
 
 	private String htmlTemplate() {
-		final StringBuilder html = new StringBuilder();
-		html.append("<h1>Vehicle Details</h1>\n");
-		html.append("<table>\n");
-		html.append("<tr><th>VIN</th><th>Blacklisted</th><th>Make</th><th>Model</th><th>Year</th><th>color</th><th>Miles</th></tr>\n");
-		html.append("<tr>\n");
-		html.append("<td>%1$s</td><td>%2$s</td><td>%3$s</td><td>%4$s</td><td>%5$s</td><td>%6$s</td><td>%7$s</td>\n");
-		html.append("</tr>\n");
-		html.append("</table>");
+		final String html = HTML_TEMPLATE;
+
 		return html.toString();
 	}
-	
+
 	private String markdownTemplate() {
 		final StringBuilder markdown = new StringBuilder();
 		markdown.append("# Vehicle Details\n");
--- src/test/java/briscoe/cleancode/VehicleFormatterTest.java
+++ src/test/java/briscoe/cleancode/VehicleFormatterTest.java
@@ -7,22 +7,15 @@ public class VehicleFormatterTest {
 
 	@Test
 	public void toHtml() {
+		final String expected = String.format(VehicleFormatter.HTML_TEMPLATE, "1HGBH41JXMN109186", true, "Honda", "civic", "2016", "BLUE", 0);
 		final String content = VehicleFormatter.byVin("1HGBH41JXMN109186").toHtml();
-		assertEquals("<h1>Vehicle Details</h1>\n" +
-		"<table>\n" +
-		"<tr><th>VIN</th><th>Blacklisted</th><th>Make</th><th>Model</th><th>Year</th><th>color</th><th>Miles</th></tr>\n" +
-		"<tr>\n" +
-		"<td>1HGBH41JXMN109186</td><td>true</td><td>Honda</td><td>civic</td><td>2016</td><td>BLUE</td><td>0</td>\n" +
-		"</tr>\n" +
-		"</table>", content);
+		assertEquals(expected, content);
 	}
 
 	@Test
 	public void toMarkdown() {
+		final String expected = String.format(VehicleFormatter.MARKDOWN_TEMPLATE, "1HGBH41JXMN109186", true, "Honda", "civic", "2016", "BLUE", 0);
 		final String content = VehicleFormatter.byVin("1HGBH41JXMN109186").toMarkdown();
-		assertEquals("# Vehicle Details\n" +
-		"VIN | Blacklisted | Make | Model | Year | color | Miles\n" +
-		"--- | --- | --- | --- | --- | --- | ---\n" +
-		"1HGBH41JXMN109186 | true | Honda | civic | 2016 | BLUE | 0", content);
+		assertEquals(expected, content);
 	}
 }
