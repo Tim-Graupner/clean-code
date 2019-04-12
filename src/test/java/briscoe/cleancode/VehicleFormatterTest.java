c9a643279458bde7c4b5dbce4553597ba60d355f
Graupner <Timothy.Graupner@adesa.com>
2019-04-12 10:53:58 -0400

    Unit testing
    Created unit tests for VehicleFormatter

--- src/test/java/briscoe/cleancode/VehicleFormatterTest.java
+++ src/test/java/briscoe/cleancode/VehicleFormatterTest.java
@@ -1,18 +1,28 @@
 package briscoe.cleancode;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class VehicleFormatterTest {
 
 	@Test
 	public void toHtml() {
 		final String content = VehicleFormatter.byVin("1HGBH41JXMN109186").toHtml();
-		System.out.println(content);
+		assertEquals("<h1>Vehicle Details</h1>\n" +
+		"<table>\n" +
+		"<tr><th>VIN</th><th>Blacklisted</th><th>Make</th><th>Model</th><th>Year</th><th>color</th><th>Miles</th></tr>\n" +
+		"<tr>\n" +
+		"<td>1HGBH41JXMN109186</td><td>true</td><td>Honda</td><td>civic</td><td>2016</td><td>BLUE</td><td>0</td>\n" +
+		"</tr>\n" +
+		"</table>", content);
 	}
-	
+
 	@Test
 	public void toMarkdown() {
 		final String content = VehicleFormatter.byVin("1HGBH41JXMN109186").toMarkdown();
-		System.out.println(content);
+		assertEquals("# Vehicle Details\n" +
+		"VIN | Blacklisted | Make | Model | Year | color | Miles\n" +
+		"--- | --- | --- | --- | --- | --- | ---\n" +
+		"1HGBH41JXMN109186 | true | Honda | civic | 2016 | BLUE | 0", content);
 	}
 }
